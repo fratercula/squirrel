48a4498a067a179fa17d702ffa320c296af269bd
LoeiFy <LoeiFy@gmail.com>
2018-09-04 13:12:06 +0800

    class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import swapper from './helper/swap.js'
+import clone from './helper/clone.js'
 
-export default swapper
+export default class {
+  constructor(data, ...uniques) {
+    this.data = data
+    this.uniques = uniques
+  }
+
+  get value() {
+    let data = clone(this.data)
+
+    for (let i = 0; i < this.uniques.length; i += 1) {
+      data = swapper(data, this.uniques[i])
+    }
+
+    return data
+  }
+
+  swap(target) {
+    let data = clone(this.data)
+
+    Object.keys(target).forEach((key) => {
+      data = swapper(data, key, target[key])
+    })
+
+    return data
+  }
+}
